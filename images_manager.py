14aa69a800d5e1e12b6b4fa2d892c47dc347524b
Ahmed Essam <theartful.ae@gmail.com>
2018-11-22 17:04:48 +0200

    Create cell and feature classes

--- images_manager.py
+++ images_manager.py
@@ -8,17 +8,60 @@ class Camera:
         assert camera_matrix.shape[1] == 4
 
         self.camera_matrix = camera_matrix
-
         # compute optical center
         m_inverse = np.linalg.inv(camera_matrix[0:3, 0:3])
         col_4 = camera_matrix[:, 3]
         self.optical_center = np.hstack((-m_inverse.dot(col_4), 1))
 
 
+class Feature:
+    def __init__(self, image_y, image_x, feature_type):
+        self.y = image_y
+        self.x = image_x
+        self.feature_type = feature_type
+
+
+class Cell:
+    def __init__(self, data):
+        self.data = data
+        self.features = []
+        self.t_patches = []
+        self.s_patches = []
+        self.depth = 0
+
+    def __getitem__(self, item):
+        return self.data[item]
+
+    def __setitem__(self, key, value):
+        self.data[key] = value
+
+
 class Image:
-    def __init__(self, data, camera=None):
+    def __init__(self, data, camera=None, cell_size=2):
         self.data = data
         self.camera = camera
+        self.cell_size = cell_size
+        [height, width, _] = data.shape
+        x_cells = width // cell_size
+        y_cells = height // cell_size
+        self.cells = [[None] * x_cells] * y_cells
+
+    def _get_cell(self, i, j):
+        """
+        private method to create cell indexed(i, j)
+        :param i: index of the cell in the y axis
+        :param j: index of the cell in the x axis
+        :return: image cell(i, j)
+        """
+        y = i * self.cell_size
+        x = j * self.cell_size
+        return Cell(self.data[y: y + self.cell_size, x: x + self.cell_size],
+                    y, x)
+
+    def cell(self, i, j):
+        if self.cells[i][j] is None:
+            self.cells[i][j] = self._get_cell(i, j)
+        return self.cells[i][j]
 
     def __getitem__(self, item):
         return self.data[item]
@@ -72,6 +115,9 @@ class ImagesManager:
         self._fundamental_matrices[(img1, img2)] = fun_mat
         self._fundamental_matrices[(img2, img1)] = fun_mat.T
         return fun_mat
+
+    def __getitem__(self, item):
+        return self.images[item]
 
     @staticmethod
     def epipole(img1, img2):
